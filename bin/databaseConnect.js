bb79f33180b65c702359cfc18e6f61e7ccc05981
jhonnold <jhonnold@mtu.edu>
2017-06-24 18:09:37 -0500

    Another minor hotfix

--- bin/databaseConnect.js
+++ bin/databaseConnect.js
@@ -10,8 +10,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
      connectionLimit : 10, //important
      host     : 'localhost',
-     user     : 'zomby',
-     password : 'm3553dup',
+     user     : 'root',
+     password : 'votouser',
      database : 'voto',
      debug    :  false
  });

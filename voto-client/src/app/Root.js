af71a82dfb256d031410819fa999c70fa03b3928
jhonnold <jhonnold@mtu.edu>
2017-07-22 00:26:53 -0500

    Moved to materialize ui and made nav/landing

---
+++ voto-client/src/app/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VotoNavBar from './components/VotoNavBar';
+import LoginPage from './containers/LoginPage';
+
+export default class Root extends React.Component {
+
+  render() {
+    return (
+      <LoginPage />
+    );
+  }
+}

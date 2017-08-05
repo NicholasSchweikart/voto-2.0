6b6cc4a0fadb41cefdb61c01a2feb3963d476437
NicholasSchweikart <nicschweikart@gmail.com>
2017-08-04 21:08:46 -0400

    "removed old DB file"

--- routes/sessions.js
+++ routes/sessions.js
@@ -10,7 +10,8 @@ const express = require('express'),
     uuidv4 = require('uuid/v4'),
     path = require('path'),
     async = require('async'),
-    AWS = require('aws-sdk');
+    AWS = require('aws-sdk'),
+    zlib = require('zlib');
 
 AWS.config.loadFromPath('./../bin/awsConfig.json');
 
@@ -149,6 +150,73 @@ router.post('/accessSession', (req, res) => {
         req.session.authorizedSessionId = sessionId;
         res.json({status: "successfully authorized"});
     })
+});
+
+/**
+ * POST route to upload new media for a specific session. Under beta right now, but will at some point need to have
+ * access to a sessionId.
+ */
+router.post('/uploadImageFiles', (req, res) => {
+
+    console.log('Attempting to receive new media uploads...');
+
+    //TODO Ensure that a user is logged in if they are uploading.
+    // if(!req.session.userId){
+    //     // Not Logged In
+    // }else if(!req.session.sessionId){
+    //     // No Session Available
+    // }else{
+    //     // We good to go
+    // }
+
+    // create an incoming form object
+    let form = new formidable.IncomingForm();
+    let filePaths = [];
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '../uploads');
+
+    // every time a file has been uploaded successfully
+    form.on('file',  (field, file)=>{
+        let newFileName = uuidv4()+'_'+file.name;
+
+        //TODO integrate amazon S3 upload here.
+        let params = {
+            Bucket:'voto-question-images',
+            Key: newFileName,
+            Body: file
+        };
+
+        s3.putObject(params,(err, data)=> {
+
+            if(err){
+                console.error(new Error("generating signed image URL: " + err));
+                return;
+            }
+            filePaths.push(newFileName);
+            console.log("S3 Return data %s", data);
+        });
+
+    });
+
+    // log any errors that occur
+    form.on('error', function (err) {
+        console.error(new Error("file upload: " + err));
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function () {
+        console.log(filePaths.length +" Uploads successful");
+        res.send({"filePaths":filePaths});
+        filePaths = [];
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+
 });
 
 /**

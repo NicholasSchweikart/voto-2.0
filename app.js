222eb8bbc04c9e3d79798b7ded3dc45b7b3facb9
Jay Honnold <jayhonnold@gmail.com>
2017-08-22 22:50:06 -0500

    200 on OPTIONS

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", 'true');
-  next();
+
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 const store = new redisStore({

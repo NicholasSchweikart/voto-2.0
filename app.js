9626526692ae9ffeb87cbd4c5ca5413d4a3a0889
NicholasSchweikart <nicschweikart@gmail.com>
2017-12-19 20:10:04 -0600

    ""

--- app.js
+++ app.js
@@ -24,7 +24,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
     next();
+  }
 });
 
 // Setup session persistence with redis

6b0ee3275e1188018d51219d2b708e82b79fb617
Jay Honnold <jayhonnold@gmail.com>
2018-01-14 15:02:53 -0500

    Added authorization in preflight

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Credentials", "true");
 
